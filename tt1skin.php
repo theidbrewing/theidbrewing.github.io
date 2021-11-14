a8aa676936d4425c201f8ece7c7e5376c487b1a9
mt8 <info@mt8.biz>
2021-11-14 22:40:26 +0900

    fix tt1skin.config.phpの存在判定ミス

--- tt1skin.php
+++ tt1skin.php
@@ -22,8 +22,8 @@ if ( ! defined( 'WPINC' ) ) {
 define( 'TT1SKIN_URL', untrailingslashit( plugin_dir_url( __FILE__ ) ) );
 define( 'TT1SKIN_PATH', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
 
-if ( is_file( 'tt1skin.config.php' ) ) {
-	require_once 'tt1skin.config.php';
+if ( file_exists( __DIR__  . '/tt1skin.config.php'  ) ) {
+	require_once __DIR__  . '/tt1skin.config.php';
 }
 // Json Controller.
 require_once TT1SKIN_PATH . '/inc/class-tt1skin-json-controller.php';
