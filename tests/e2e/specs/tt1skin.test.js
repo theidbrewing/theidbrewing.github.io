bb48c2ea9d0acad34c19cfc802c5604be66ab317
mimi <mimitips@gmail.com>
2021-11-25 23:58:52 +0900

    test / Add heading block style test

--- tests/e2e/specs/tt1skin.test.js
+++ tests/e2e/specs/tt1skin.test.js
@@ -1,10 +1,25 @@
-import { visitAdminPage,createURL } from '@wordpress/e2e-test-utils';
+import { visitAdminPage, createNewPost, clickBlockAppender, getEditedPostContent, activateTheme } from '@wordpress/e2e-test-utils';
 
 describe('TT1 Skin', () => {
 
-    it('Sample Test', async () => {
-        await visitAdminPage( '/' );
-        expect(true).toEqual(true);
+    // it('Sample Test', async () => {
+    //     await visitAdminPage( '/' );
+    //     expect(true).toEqual(true);
+    // });
+
+    beforeEach(async () => {
+        await activateTheme('twentytwentyone');
+        await createNewPost();
+    });
+
+    it('should have the heading block custom style', async () => {
+        await clickBlockAppender();
+        await page.keyboard.type('## 2');
+        //select custom style
+        await page.click(
+            `.block-editor-block-styles__item[aria-label="section title"]`
+        );
+        expect(await getEditedPostContent()).toMatchSnapshot();
     });
 
 });
