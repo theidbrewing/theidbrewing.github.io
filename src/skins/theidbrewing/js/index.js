298cf475b49ee6e558a34f1304b3b8dd214664c7
mimi <mimitips@gmail.com>
2022-03-18 14:29:15 +0900

    feature / Add loading animations

--- src/skins/theidbrewing/js/index.js
+++ src/skins/theidbrewing/js/index.js
@@ -1,5 +1,11 @@
-window.addEventListener('load', function() {
+document.addEventListener("readystatechange", function() {
+// --  body に is-loading を渡す
+    var myBody = document.getElementsByTagName('body');
+    myBody[0].classList.add('is-loading');
+})
+document.addEventListener('DOMContentLoaded', function() {
     // --  body に is-loaded を渡す
     var myBody = document.getElementsByTagName('body');
+    myBody[0].classList.remove('is-loading');
     myBody[0].classList.add('is-loaded');
 })
